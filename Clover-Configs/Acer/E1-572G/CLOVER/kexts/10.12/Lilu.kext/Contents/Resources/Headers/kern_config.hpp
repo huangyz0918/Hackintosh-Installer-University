149f9569292d27a7c93a1da71382cb395b5c74db
huangyz0918 <huangyz0918@gmail.com>
2018-10-27 17:17:51 +0800

    ✨ feat:
    added clover about Aser.

---
+++ Clover-Configs/Acer/E1-572G/CLOVER/kexts/10.12/Lilu.kext/Contents/Resources/Headers/kern_config.hpp
@@ -0,0 +1,15 @@
+//
+//  kern_config.hpp
+//  Lilu
+//
+//  Copyright © 2016-2017 vit9696. All rights reserved.
+//
+
+#ifndef kern_config_hpp
+#define kern_config_hpp
+
+// As for now claim to support all the features
+#define KEXTPATCH_SUPPORT 1
+#define COMPRESSION_SUPPORT 1
+
+#endif /* kern_config_hpp */
